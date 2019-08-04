a466ba27bea65c36fb08424e669d4506a4e29fd7
Code_Miles <devaze007@gmail.com>
2019-08-04 15:56:37 +0100

    finished functionality

--- redditApi.js
+++ redditApi.js
@@ -1,6 +1,6 @@
 export default{
     search: function(searchTerm){
-     return fetch(`http://www.reddit.com/search.json?q=${searchTerm}&limit=100&sort=${sortby}`)
+     return fetch(`http://www.reddit.com/search.json?q=${searchTerm}&limit=100&sort=hot`)
       .then(res => res.json())
       .then(data => data.data.children.map(data =>
           data.data)) 
